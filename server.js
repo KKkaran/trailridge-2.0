478f32e411a1c27a8cb82208bc6919aaa2d860f7
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2022-01-22 19:47:35 -0500

    all required files and folders created

---
+++ server.js
@@ -0,0 +1,14 @@
+//this is the server file
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.get("/",(req,res)=>{
+    res.send("working now")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`listening on ${PORT}`)
+})
